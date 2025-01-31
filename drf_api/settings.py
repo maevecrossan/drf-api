2b06911aa795302bc3772c857859fe4820ea33e8
Maeve Crossan <maevecrossan@gmail.com>
2025-01-31 16:13:24 +0000

    Downgrade django version to 3.2.4

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -104,6 +104,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
