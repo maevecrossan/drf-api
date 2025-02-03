e89fef960981bcd8c68c64f68176df7589ff278d
Maeve Crossan <maevecrossan@gmail.com>
2025-02-03 13:08:10 +0000

    Update allowed hosts to include heroku link. Update settings.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,9 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import re
 from datetime import timezone
+import dj_database_url
 
 
 if os.path.exists('env.py'):
@@ -69,7 +68,8 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
    os.environ.get('ALLOWED_HOST'),
    '8000-maevecrossan-drfapi-epo4cmcdtmt.ws.codeinstitute-ide.net',
-   'localhost'
+   'localhost',
+   'https://drf-test-3b810d58330a.herokuapp.com/profiles/',
 ]
 
 
@@ -151,17 +151,14 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-     }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    'default': ({
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
+}
 
 
 # Password validation
