83b3b66a06cb92e190dcd1138376153a229a2252
Maeve Crossan <maevecrossan@gmail.com>
2025-02-07 08:55:00 +0000

    Update db settings. Add additional heroku app link. Migrate to database.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -70,6 +70,7 @@ ALLOWED_HOSTS = [
    '8000-maevecrossan-drfapi-epo4cmcdtmt.ws.codeinstitute-ide.net',
    'localhost',
    'drf-test-3b810d58330a.herokuapp.com',
+   'https://drf-test-3b810d58330a.herokuapp.com/',
 ]
 
 
@@ -151,14 +152,17 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': ({
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    ))
-}
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
