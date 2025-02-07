c8145ef32c0f7ce6e7b7d5977787d09e4a6c6d63
Maeve Crossan <maevecrossan@gmail.com>
2025-02-07 11:27:44 +0000

    fix typo in post serializer.

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,7 +10,7 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.proile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
     comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
